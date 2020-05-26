2b502c3d9eb388d910804b5febeb7bdfb12afd7e
Udius <65825225+Udius@users.noreply.github.com>
2020-05-26 20:01:32 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
-from pickle import load
+POST_ID = []
 
-with open('POST_ID', 'rb') as psts_ids:
-	POST_ID = load(psts_ids)
-with open('ROLES', 'rb') as rls:
-	ROLES = load(rls)
+ROLES = {}
 
 EXCROLES = {}
 
-HIGHTROLES = [714082027019698207, 714423604615315486]
+HIGHTROLES = [714082027019698207]
 
 MAX_ROLES_PER_USER = 3333
